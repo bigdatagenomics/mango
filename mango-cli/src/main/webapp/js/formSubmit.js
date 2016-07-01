153745d7b59bb91aa404adeb34e915606b8b6896
Alyssa Morrow <akmorrow@berkeley.edu>
2016-07-01 14:36:08 -0700

    home page and navigation now works with pileup

--- mango-cli/src/main/webapp/js/formSubmit.js
+++ mango-cli/src/main/webapp/js/formSubmit.js
@@ -14,39 +14,6 @@ function autoComplete(dictionary) {
     });
 }
 
-// Get and validate form info, reference regions
-function validateFormElements(form) {
-  var info = form.info.value;
-
-  // validate that input has correct form
-  try {
-      var refName = info.split(":")[0];
-      var region = info.split(":")[1].split("-");
-      var newStart = Math.max(0, region[0]);
-      var newEnd = Math.max(newStart, region[1]);
-  }
-  catch(err) {
-      form[0].style.borderColor = "red";
-      return undefined;
-  }
-
-  // Check that the each form input is correct
-  if ( info===""|| refName==""|| isNaN(newStart) || isNaN(newEnd) || newStart>=newEnd){
-    form[0].style.borderColor = "red";
-    return undefined;
-  }
-  else{
-    form[0].style.borderColor = "";
-    setGlobalReferenceRegion(refName, newStart, newEnd);
-
-    return {
-            refName: refName,
-            newStart: newStart,
-            newEnd: newEnd
-        };
-  }
-}
-
 // Redirect based on form input
 function checkForm(form) {
   var elements = validateFormElements(form);
