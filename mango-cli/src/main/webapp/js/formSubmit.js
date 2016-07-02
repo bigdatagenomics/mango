70e2b43f15d2bc8a91d331e75459a13e12d43feb
Alyssa Morrow <akmorrow@berkeley.edu>
2016-07-01 17:19:43 -0700

    removed unneeded dependencies and files after replacement with pileup

--- mango-cli/src/main/webapp/js/formSubmit.js
+++ mango-cli/src/main/webapp/js/formSubmit.js
@@ -3,98 +3,33 @@
 * rendering
 ***/
 
-// Autocomplete function for input submit forms
-function autoComplete(dictionary) {
-    $('#autocomplete').autocomplete({
-      lookup: dictionary,
-      onSelect: function (suggestion) {
-        var thehtml = '<strong>Name:</strong> '+suggestion.value;
-        $('#outputcontent').html(thehtml);
-      }
-    });
-}
+// Validates user input for form
+$('.controls>button').click(function(){
+    validateFormElements();
+});
 
-// Redirect based on form input
-function checkForm(form) {
-  var elements = validateFormElements(form);
-  if (elements != undefined) {
-    render(elements.refName, elements.newStart, elements.newEnd);
+function validateFormElements() {
+
+  // Validate input form syntax
+  var form = $("#form-field");
+  var region = form.val().split("-");
+
+  try {
+      var newStart = Math.max(0, region[0]);
+      var newEnd = Math.max(newStart, region[1]);
   }
-}
-
-// Try to move very far left
-function moveVeryFarLeft() {
-  if (validRegion()) {
-    var newStart = Math.max(0, viewRegStart - (viewRegEnd-viewRegStart));
-    var newEnd = Math.max(newStart, viewRegEnd - (viewRegEnd-viewRegStart));
-    render(viewRefName, newStart, newEnd);
-  }
-}
-
-// Try to move far left
-function moveFarLeft() {
-  if (validRegion()) {
-    var newStart = Math.max(0, viewRegStart - Math.floor((viewRegEnd-viewRegStart)/2));
-    var newEnd = Math.max(newStart, viewRegEnd - Math.floor((viewRegEnd-viewRegStart)/2));
-    render(viewRefName, newStart, newEnd);
-  }
-}
-
-// Try to move left
-function moveLeft() {
-  if (validRegion()) {
-    var newStart = Math.max(0, viewRegStart - Math.floor((viewRegEnd-viewRegStart)/4));
-    var newEnd = Math.max(newStart, viewRegEnd - Math.floor((viewRegEnd-viewRegStart)/4));
-    render(viewRefName, newStart, newEnd);
-  }
-}
-
-// Try to move right
-function moveRight() {
- if (validRegion()) {
-   var newStart = viewRegStart + Math.floor((viewRegEnd-viewRegStart)/4);
-   var newEnd = viewRegEnd + Math.floor((viewRegEnd-viewRegStart)/4);
-   render(viewRefName, newStart, newEnd);
- }
-}
-
-// Try to move far right
-function moveFarRight() {
-  if (validRegion()) {
-    var newStart = viewRegStart + Math.floor((viewRegEnd-viewRegStart)/2);
-    var newEnd = viewRegEnd + Math.floor((viewRegEnd-viewRegStart)/2);
-    render(viewRefName, newStart, newEnd);
-  }
-}
-
-// Try to move very far right
-function moveVeryFarRight() {
-  if (validRegion()) {
-    var newStart = viewRegStart + (viewRegEnd-viewRegStart);
-    var newEnd = viewRegEnd + (viewRegEnd-viewRegStart);
-    render(viewRefName, newStart, newEnd);
-  }
-}
-
-// Try to zoom in
-function zoomIn() {
-  if (validRegion()) {
-    var newStart = viewRegStart + Math.floor((viewRegEnd-viewRegStart)/4);
-    var newEnd = viewRegEnd - Math.floor((viewRegEnd-viewRegStart)/4);
-    render(viewRefName, newStart, newEnd);
-  }
-}
-
-// Try to zoom out
-function zoomOut() {
-  if (validRegion()) {
-    var newStart = Math.max(0, viewRegStart - Math.floor((viewRegEnd-viewRegStart)/2));
-    var newEnd = viewRegEnd + Math.floor((viewRegEnd-viewRegStart)/2);
-    render(viewRefName, newStart, newEnd);
+  catch(err) {
+      form[0].style.borderColor = "red";
   }
 
+  // Check that the each form input is correct
+  if ( region===""|| isNaN(newStart) || isNaN(newEnd) || newStart>=newEnd){
+    form[0].style.borderColor = "red";
+  }
+  else{
+    form[0].style.borderColor = "";
+  }
 }
-
 /* Validates whether form query has defined start, end and name elements
  * @return true if valid elements, false if invalid elements
  */
