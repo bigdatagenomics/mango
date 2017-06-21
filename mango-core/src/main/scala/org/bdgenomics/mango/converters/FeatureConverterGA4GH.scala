bfecdb90e9e0f516122391e16e1b210e2caad78d
Paschall <paschalj@HG-01931188-LM1.fios-router.home>
2017-06-20 22:19:11 -0400

    split up GA4GH converters and improvements therein

---
+++ mango-core/src/main/scala/org/bdgenomics/mango/converters/FeatureConverterGA4GH.scala
@@ -0,0 +1,8 @@
+package org.bdgenomics.mango.converters
+
+/**
+  * Created by paschalj on 6/20/17.
+  */
+object FeatureConverterGA4GH {
+
+}
---
+++ mango-core/src/main/scala/org/bdgenomics/mango/converters/VariantContextCoverterGA4GH.scala
@@ -0,0 +1,8 @@
+package org.bdgenomics.mango.converters
+
+/**
+  * Created by paschalj on 6/20/17.
+  */
+object VariantContextCoverterGA4GH {
+
+}
