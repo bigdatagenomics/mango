c1907df2815a0f72cc4eec2f4fa42f6370bc1e3a
Paschall <paschalj@HG-01931188-LM1.fios-router.home>
2017-08-21 16:25:34 -0400

    address review comments

---
+++ mango-core/src/main/scala/org/bdgenomics/mango/converters/ga4gh/FeatureConverterGA4GH.scala
@@ -0,0 +1,69 @@
+/**
+ * Licensed to Big Data Genomics (BDG) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The BDG licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bdgenomics.mango.converters.ga4gh
+
+import java.lang.Boolean
+import java.util
+
+import com.google.protobuf.ListValue
+import htsjdk.samtools.{ CigarOperator, TextCigarCodec, ValidationStringency }
+import org.bdgenomics.adam.models.VariantContext
+import org.bdgenomics.adam.rdd.ADAMContext._
+import org.bdgenomics.formats.avro._
+import org.bdgenomics.utils.misc.Logging
+import org.ga4gh._
+import ga4gh.Variants.Call
+import ga4gh.{ Common, Variants }
+
+import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
+
+/**
+ * Created by paschalj on 6/20/17.
+ */
+object FeatureConverterGA4GH extends Serializable with Logging {
+
+  def toGA4GHFeature(record: Feature): ga4gh.SequenceAnnotations.Feature = {
+
+    val x: Strand = record.getStrand
+
+    def bdgStrandtoGA4GH(strand: Strand): ga4gh.Common.Strand = {
+      if (strand == Strand.FORWARD) ga4gh.Common.Strand.POS_STRAND
+      else if (strand == Strand.REVERSE) ga4gh.Common.Strand.NEG_STRAND
+      else if (strand == Strand.UNKNOWN) ga4gh.Common.Strand.UNRECOGNIZED
+      else ga4gh.Common.Strand.STRAND_UNSPECIFIED
+    }
+
+    def bdgFeatureTypeTermToGA4GH(featureType: String): ga4gh.Common.OntologyTerm = {
+      ga4gh.Common.OntologyTerm.newBuilder().setTermId(featureType).build()
+    }
+
+    ga4gh.SequenceAnnotations.Feature.newBuilder()
+      .setStart(record.getStart)
+      .setEnd(record.getEnd)
+      .setStrand(bdgStrandtoGA4GH(record.getStrand))
+      .setReferenceName(record.getContigName)
+      .setFeatureType(bdgFeatureTypeTermToGA4GH(record.getFeatureType))
+      .setAttributes(Common.Attributes.newBuilder()
+        .putAttr("Score", Common.AttributeValueList.newBuilder()
+          .addValues(0, Common.AttributeValue.newBuilder().setInt64Value(record.getScore.toLong)).build()))
+      .build()
+
+  }
+
+}
---
+++ mango-core/src/main/scala/org/bdgenomics/mango/converters/ga4gh/VariantContextCoverterGA4GH.scala
@@ -0,0 +1,187 @@
+/**
+ * Licensed to Big Data Genomics (BDG) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The BDG licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bdgenomics.mango.converters.ga4gh
+
+import java.lang.Boolean
+import java.util
+
+import com.google.protobuf.ListValue
+import htsjdk.samtools.{ CigarOperator, TextCigarCodec, ValidationStringency }
+import org.bdgenomics.adam.models.VariantContext
+import org.bdgenomics.adam.rdd.ADAMContext._
+import org.bdgenomics.formats.avro.{ AlignmentRecord, Feature, Genotype, GenotypeAllele }
+import org.bdgenomics.utils.misc.Logging
+import org.ga4gh._
+import ga4gh.Variants.Call
+import ga4gh.{ Common, Variants }
+//import org.bdgenomics.mango.converters.GA4GHConverter.toGA4GHCall
+import java.lang.Double
+
+import ga4gh.VariantServiceOuterClass.{ SearchCallSetsResponse, SearchVariantsResponse }
+
+import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
+
+/**
+ * Created by paschalj on 6/20/17.
+ */
+object VariantContextCoverterGA4GH extends RecordConverterGA4GH[VariantContext, ga4gh.Variants.Variant] {
+
+  override def bdgToGA4GH(record: VariantContext): ga4gh.Variants.Variant = {
+    bdgToGA4GH(record: VariantContext, variantSetID = "")
+  }
+
+  def bdgToGA4GH(record: VariantContext, variantSetID: String = ""): ga4gh.Variants.Variant = {
+
+    val ga4ghCalls: Iterable[Call] = record.genotypes.map(g => toGA4GHCall(g))
+
+    ga4gh.Variants.Variant.newBuilder()
+      .setId(record.variant.variant.getNames.head)
+      .setVariantSetId(variantSetID)
+      .addAllNames(record.variant.variant.getNames())
+      .setReferenceName(record.variant.variant.getContigName)
+      .setStart(record.variant.variant.getStart)
+      .setEnd(record.variant.variant.getEnd)
+      .setReferenceBases(record.variant.variant.getReferenceAllele)
+      .addAllAlternateBases(List(record.variant.variant.getAlternateAllele)) // note: VariantContext is defined only to have one altallele
+      .addAllCalls(ga4ghCalls.asJava)
+      .addAllFiltersFailed(record.variant.variant.getFiltersFailed)
+      .setFiltersPassed(record.variant.variant.getFiltersPassed)
+      .setFiltersApplied(record.variant.variant.getFiltersApplied)
+      .build()
+  }
+
+  def toGA4GHCall(record: Genotype, callsetID: String = "NA"): ga4gh.Variants.Call = {
+
+    def toGA4GHAllele(input: GenotypeAllele): String = {
+      if (input == GenotypeAllele.REF) "0"
+      else if (input == GenotypeAllele.ALT) "1"
+      else "."
+    }
+
+    val inputAlleles: util.List[GenotypeAllele] = record.getAlleles
+
+    val alleleFirst = toGA4GHAllele(inputAlleles(0))
+    val alleleSecond = toGA4GHAllele(inputAlleles(1))
+
+    val genotypeAlleles = List(alleleFirst, alleleSecond)
+
+    val alleleFirstValue = com.google.protobuf.Value.newBuilder().setStringValue(alleleFirst)
+    val alleleSecondValue = com.google.protobuf.Value.newBuilder().setStringValue(alleleSecond)
+
+    val calls = ListValue.newBuilder().addValues(alleleFirstValue)
+      .addValues(alleleSecondValue)
+
+    // There has gotta be a better way, but compiler was not happy
+    val gl: List[java.lang.Double] = record.getGenotypeLikelihoods.map((x) => { val y: java.lang.Double = x.toDouble; y }).toList
+
+    var builder = ga4gh.Variants.Call.newBuilder()
+      .setCallSetName(record.getSampleId)
+      .setCallSetId(callsetID)
+      .setGenotype(calls)
+
+    if (record.getPhased) {
+      builder = builder.setPhaseset(record.getPhaseSetId.toString)
+    }
+
+    if (!gl.isEmpty) {
+      builder = builder.addAllGenotypeLikelihood(gl)
+    }
+
+    builder.build()
+
+  }
+
+  override def ga4ghSeqtoJSON(data: Seq[ga4gh.Variants.Variant]): String = {
+    val result: SearchVariantsResponse = ga4gh.VariantServiceOuterClass.SearchVariantsResponse.newBuilder()
+      .addAllVariants(data.asJava).build()
+    com.google.protobuf.util.JsonFormat.printer().print(result)
+
+  }
+
+  /*
+  def toGA4GHVariant(record: VariantContext, variantID: String = "", variantSetID: String = ""): ga4gh.Variants.Variant = {
+
+    val ga4ghCalls: Iterable[Call] = record.genotypes.map(g => toGA4GHCall(g))
+
+    ga4gh.Variants.Variant.newBuilder()
+      .setId(variantID)
+      .setVariantSetId(variantSetID)
+      .addAllNames(record.variant.variant.getNames())
+      .setReferenceName(record.variant.variant.getContigName)
+      .setStart(record.variant.variant.getStart)
+      .setEnd(record.variant.variant.getEnd)
+      .setReferenceBases(record.variant.variant.getReferenceAllele)
+      .addAllAlternateBases(List(record.variant.variant.getAlternateAllele)) // note: VariantContext is defined only to have one altallele
+      .addAllCalls(ga4ghCalls.asJava)
+      .addAllFiltersFailed(record.variant.variant.getFiltersFailed)
+      .setFiltersPassed(record.variant.variant.getFiltersPassed)
+      .setFiltersApplied(record.variant.variant.getFiltersApplied)
+      .build()
+  }
+
+  def toGA4GHCall(record: Genotype, callsetID: String = "NA"): ga4gh.Variants.Call = {
+
+    def toGA4GHAllele(input: GenotypeAllele): String = {
+      if (input == GenotypeAllele.REF) "0"
+      else if (input == GenotypeAllele.ALT) "1"
+      else "."
+    }
+
+    val inputAlleles: util.List[GenotypeAllele] = record.getAlleles
+
+    val alleleFirst = toGA4GHAllele(inputAlleles(0))
+    val alleleSecond = toGA4GHAllele(inputAlleles(1))
+
+    val genotypeAlleles = List(alleleFirst, alleleSecond)
+
+    val alleleFirstValue = com.google.protobuf.Value.newBuilder().setStringValue(alleleFirst)
+    val alleleSecondValue = com.google.protobuf.Value.newBuilder().setStringValue(alleleSecond)
+
+    val calls = ListValue.newBuilder().addValues(alleleFirstValue)
+      .addValues(alleleSecondValue)
+
+    // There has gotta be a better way, but compiler was not happy
+    val gl: List[java.lang.Double] = record.getGenotypeLikelihoods.map((x) => { val y: java.lang.Double = x.toDouble; y }).toList
+
+    var builder = ga4gh.Variants.Call.newBuilder()
+      .setCallSetName(record.getSampleId)
+      .setCallSetId(callsetID)
+      .setGenotype(calls)
+
+    if (record.getPhased) {
+      builder = builder.setPhaseset(record.getPhaseSetId.toString)
+    }
+
+    if (!gl.isEmpty) {
+      builder = builder.addAllGenotypeLikelihood(gl)
+    }
+
+    builder.build()
+
+  }
+
+  def listGApbToJson(data: Seq[ga4gh.Variants.Variant]): String = {
+    val result: SearchVariantsResponse = ga4gh.VariantServiceOuterClass.SearchVariantsResponse.newBuilder()
+      .addAllVariants(data.asJava).build()
+    com.google.protobuf.util.JsonFormat.printer().print(result)
+
+  }
+
+*/
+}
