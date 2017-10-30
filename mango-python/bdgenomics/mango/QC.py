8282de60191c436a7551be13167577f1cea5b9de
George He <georgehe@berkeley.edu>
2017-10-29 23:13:23 -0400

    Update ipynb

--- mango-python/bdgenomics/mango/QC.py
+++ mango-python/bdgenomics/mango/QC.py
@@ -18,9 +18,6 @@
 
 from collections import Counter, OrderedDict
 from cigar import Cigar
-import matplotlib
-# Disable x-forwarding
-matplotlib.use('agg')
 import matplotlib.pyplot as plt
 plt.rcdefaults()
 
