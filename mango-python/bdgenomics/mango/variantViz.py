15ef765006ebc506d1450acb953be500a9d490c4
Justin Pascall <justinpaschalldna@gmail.com>
2018-04-20 14:46:56 -0400

    added count variant by gene graph

--- mango-python/bdgenomics/mango/variantViz.py
+++ mango-python/bdgenomics/mango/variantViz.py
@@ -25,44 +25,44 @@ import math
 plt.rcdefaults()
 
 
+so_terms = [
+    'intergenic_region',
+    'downstream_gene_variant',
+    'upstream_gene_variant',
+    'sequence_feature',
+    'structural_interaction_variant',
+    'intron_variant',
+    'non_coding_transcript_exon_variant',
+    '3_prime_UTR_variant',
+    '5_prime_UTR_variant',
+    'synonymous_variant',
+    'conservative_inframe_insertion',
+    'conservative_inframe_deletion',
+    'initiator_codon_variant',
+    'protein_protein_contact',
+    'non_coding_transcript_variant',
+    'stop_retained_variant',
+    '5_prime_UTR_premature_start_codon_gain_variant',
+    'disruptive_inframe_deletion',
+    'disruptive_inframe_insertion',
+    'missense_variant',
+    'stop_lost',
+    'start_lost',
+    'splice_donor_variant',
+    'splice_acceptor_variant',
+    'splice_region_variant',
+    'frameshift_variant',
+    'stop_gained']
+
+so_to_rank = dict(zip(so_terms, range(1,len(so_terms)+1)))
+rank_to_so = {v: k for k, v in so_to_rank.iteritems()}
+
+def most_sig_effect(effectList):
+    return rank_to_so[max( [ so_to_rank[item] for item in effectList] )]
+
 class VariantEffectAlleleFreq(object):
 
     def __init__(self, ss, variantRDD, annot_list=[], bins=100):
-
-        so_terms = [
-            'intergenic_region',
-            'downstream_gene_variant',
-            'upstream_gene_variant',
-            'sequence_feature',
-            'structural_interaction_variant',
-            'intron_variant',
-            'non_coding_transcript_exon_variant',
-            '3_prime_UTR_variant',
-            '5_prime_UTR_variant',
-            'synonymous_variant',
-            'conservative_inframe_insertion',
-            'conservative_inframe_deletion',
-            'initiator_codon_variant',
-            'protein_protein_contact',
-            'non_coding_transcript_variant',
-            'stop_retained_variant',
-            '5_prime_UTR_premature_start_codon_gain_variant',
-            'disruptive_inframe_deletion',
-            'disruptive_inframe_insertion',
-            'missense_variant',
-            'stop_lost',
-            'start_lost',
-            'splice_donor_variant',
-            'splice_acceptor_variant',
-            'splice_region_variant',
-            'frameshift_variant',
-            'stop_gained']
-
-        so_to_rank = dict(zip(so_terms, range(1,len(so_terms)+1)))
-        rank_to_so = {v: k for k, v in so_to_rank.iteritems()}
-
-        def most_sig_effect(effectList):
-            return rank_to_so[max( [ so_to_rank[item] for item in effectList] )]
 
         def jp3(dd1):
             effectList = [ effect for transcript in dd1['transcriptEffects'] for effect in transcript['effects'] ]
@@ -102,16 +102,57 @@ class VariantEffectAlleleFreq(object):
         plt.yscale('log')
         plt.show()
 
+
+
+class VariantCountByPop(object):
+    def __init__(self, ss, variantRDD, annot_list=[], bins=100):
+
+        def jp3(dd1):
+            effectList = [ effect for transcript in dd1['transcriptEffects'] for effect in transcript['effects'] ]
+            mostSigEffect = most_sig_effect(effectList)
+            return mostSigEffect
+
+        if len(annot_list) == 0:
+            annot_list = ['frameshift_variant','stop_gained', 'missense_variant', 'synonymous_variant']
+
+        data_bins = []
+        data_weights = []
+
+        pop_list = [ 'AF_NFE', 'AF_AFR']
+        for pop in pop_list:
+           curr = variantRDD.toDF().rdd.map(lambda w: w.asDict()).map(lambda p: p['annotation']).filter(
+             lambda x: x['attributes'][pop] != ".").map(
+             lambda d: (jp3(d), math.floor(float(d['attributes'][pop])*100000)/100000)).filter(lambda f: f[1] > 0).countByValue()
+           currlist = curr.items()
+           currbins = [ item[0][1] for item in currlist ]
+           currweights = [ item[1] for item in currlist ]
+           data_bins.append(currbins)
+           data_weights.append(currweights)
+
+
+
+
+        self.data_bins = data_bins
+        self.data_weights = data_weights
+        self.annot_list = annot_list
+        self.pop_list=pop_list
+        self.bins = bins
+        self.sc = ss.sparkContext
+
+    def plot(self):
+        plt.rcdefaults()
+        plt.title("Allele Frequency Distribution by Population")
+        plt.hist(self.data_bins, weights=self.data_weights, bins=self.bins, histtype = 'step', label=self.pop_list)
+        plt.legend()
+        plt.yscale('log')
+        #plt.xscale('log')
+        plt.show()
+
+
+
 class VariantEffectCounts(object):
 
     def __init__(self, ss, variantRDD, includeList = []):
-        """
-        Initializes a AlignmentDistribution class.
-        Computes the alignment distribution of multiple coverageRDDs.
-        :param SparkContext: the global SparkContext
-        :param alignmentRDDs: A list of bdgenomics.adam.rdd.AlignmentRDD objects
-        :param int bin_size: Division size per bin
-        """
         if not includeList:
             includeList = [
                 u'non_coding_transcript_exon_variant',
@@ -142,14 +183,7 @@ class VariantEffectCounts(object):
         self.effectCounts = effectCounts
 
 
-    def plot(self, xScaleLog = False, yScaleLog = False, testMode = False, plotType="I"):
-        """
-        Plots final distribution values and returns the plotted distribution as a counter object.
-        :param xScaleLog: rescales xaxis to log
-        :param yScaleLog: rescales yaxis to log
-        :param testMode: if true, does not generate plot. Used for testing.
-        :param plotType: Cigar type to plot, from ['I', 'H', 'D', 'M', 'S']
-        """
+    def plot(self):
 
         plt.rcdefaults()
         fig, ax = plt.subplots()
@@ -163,5 +197,28 @@ class VariantEffectCounts(object):
         plt.title("Variant Count By Functional Consequence")
         plt.show()
 
+class VariantCountByGene(object):
+    def __init__(self,ss,variantRDD, so_term='missense_variant', bins=100):
 
+      def compute(dd1):
+        effectList = [ effect for transcript in dd1['transcriptEffects'] for effect in transcript['effects'] ]
+        geneList = [ transcript['geneId'] for transcript in dd1['transcriptEffects'] ]
+        if len(geneList)==0:
+          geneList = list("NoGene")
+        mostSigEffect = most_sig_effect(effectList)
+        return [mostSigEffect, geneList[0]]
+
+      y= variantRDD.toDF().rdd.map(lambda w: w.asDict()).map(lambda p: p['annotation']).filter(
+        lambda x: x['attributes']['AF_NFE'] != ".").map(
+        lambda d: ( compute(d)[0],compute(d)[1])).filter(
+        lambda f: f[0] == so_term and f[1] != 'NoGene').countByValue()
+
+      self.data = y.values()
+      self.bins = bins
+      self.so_term = so_term
+
+    def plot(self):
+        plt.hist(self.data, bins=100)
+        plt.title("Count of " + self.so_term + " per gene")
+        plt.show()
 
