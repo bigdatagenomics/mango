8007bd5ca0dcbe5987b1a16a6a151bae81805ab2
Justin Pascall <justinpaschalldna@gmail.com>
2018-11-17 11:10:15 -0500

    Added more genotype visualizations

--- mango-python/bdgenomics/mango/genotypes.py
+++ mango-python/bdgenomics/mango/genotypes.py
@@ -26,9 +26,12 @@ Genotypes
 
    GenotypeSummary
    VariantsPerSampleDistribution
+   HetHomRatioDistribution
+   GenotypeCallRatesDistribution
 """
 
 from bdgenomics.mango.pileup.track import *
+from pyspark.sql.functions import col, expr, when
 
 from .distribution import HistogramDistribution
 import matplotlib.pyplot as plt
@@ -62,6 +65,8 @@ class GenotypeSummary(object):
 
         # where to store collected data
         self.variantsPerSampleDistribution = None
+        self.hetHomRatioDistribution = None
+        self.genotypeCallRatesDistribution = None
 
     def getVariantsPerSampleDistribution(self):
         """
@@ -80,6 +85,31 @@ class GenotypeSummary(object):
 
         return self.variantsPerSampleDistribution
 
+    def getHetHomRatioDistribution(self):
+        """
+        Computes a distribution of (Heterozygote/Homozygote) ratios per sample
+
+        Returns:
+           HetHomRatioDistribution object
+        """
+        if self.hetHomRatioDistribution == None:
+            print("Computing (Heterozygote/Homozygote) ratio distribution")
+            self.hetHomRatioDistribution = HetHomRatioDistribution(self.ss, self.genotypeRdd, self.sample)
+
+        return self.hetHomRatioDistribution
+
+    def getGenotypeCallRatesDistribution(self):
+        """
+        Computes a distribution of variant CallRate (called / total_variants) per sample
+
+        Returns:
+           GenotypeCallRatesDistribution object
+        """
+        if self.genotypeCallRatesDistribution == None:
+            print("Computing per sample callrate distribution")
+            self.genotypeCallRatesDistribution = GenotypeCallRatesDistribution(self.ss, self.genotypeRdd, self.sample)
+
+        return self.genotypeCallRatesDistribution
 
 class VariantsPerSampleDistribution(HistogramDistribution):
     """ VariantsPerSampleDistribution class.
@@ -94,7 +124,7 @@ class VariantsPerSampleDistribution(HistogramDistribution):
         Args:
             :param ss: global SparkSession.
             :param genotypeRDD: A bdgenomics.adam.rdd.GenotypeRDD object.
-            :param sample: Fraction to sample CoverageRDD. Should be between 0 and 1
+            :param sample: Fraction to sample GenotypeRDD. Should be between 0 and 1
         """
 
         self.sc = ss.sparkContext
@@ -105,3 +135,117 @@ class VariantsPerSampleDistribution(HistogramDistribution):
             .map(lambda r: ((r["sampleId"]), 1))
 
         HistogramDistribution.__init__(self)
+
+class HetHomRatioDistribution(object):
+    """ HetHomRatioDistribution class.
+    HetHomRatioDistribution computes a distribution of (Heterozygote/Homozygote) ratios from a genotypeRDD.
+    """
+    def __init__(self,ss,genotypeRDD, sample = 1.0):
+        """
+        Initializes HetHomRatioDistribution class.
+        Retrieves the call rate and missing rate for each sample in a genotypeRDD
+
+        Args:
+            :param ss: global SparkSession.
+            :param genotypeRDD: A bdgenomics.adam.rdd.GenotypeRDD object.
+            :param sample: Fraction to sample GenotypeRDD. Should be between 0 and 1
+        """
+
+        self.sample = sample
+        new_col1 = when((col("alleles")[0] == u'REF') & (col("alleles")[1] == u'ALT'),1) \
+            .otherwise(when( (col("alleles")[0] == u'ALT') & (col("alleles")[1] == u'ALT'),2))
+
+        genocounts =  genotypeRDD.toDF().sample(False, self.sample) \
+            .withColumn("hethomclass", new_col1) \
+            .groupBy("sampleid", "hethomclass").count().collect()
+
+        data_het = {}
+        data_hom = {}
+        for row in genocounts:
+            curr = row.asDict()
+            if(curr['hethomclass'] == 1):
+              data_het[curr['sampleid']] = curr['count']
+            if(curr['hethomclass'] == 2):
+              data_hom[curr['sampleid']] = curr['count']
+
+        self.hetHomRatio = []
+        for sampleid in data_hom.keys():
+            if sampleid in data_het.keys():
+                self.hetHomRatio.append(float(data_het[sampleid])/float(data_hom[sampleid]))
+
+    def plot(self, testMode = False, **kwargs):
+        """
+        Plots final distribution values and returns the plotted distribution as a list
+
+        Returns:
+          matplotlib axis to plot and computed data
+        """
+
+        if(not testMode):
+            figsize = kwargs.get('figsize',(10,5))
+            fig, ax = plt.subplots(figsize=figsize)
+            hist = ax.hist(self.hetHomRatio,bins=100)
+            return ax, self.hetHomRatio
+        else:
+            return None, self.hetHomRatio
+
+
+class GenotypeCallRatesDistribution(object):
+    """ GenotypeCallRatesDistribution class.
+    GenotypeCallRatesDistribution computes a distribution of per-sample genotype call rates from a genotypeRDD.
+    """
+    def __init__(self,ss,genotypeRDD, sample = 1.0):
+        """
+        Initializes a GenotypeCallRatesDistribution class.
+        Retrieves counts of called and missing genotypes from a genotypeRDD.
+
+        Args:
+            :param ss: SparkContext
+            :param genotypeRDD: bdgenomics genotypeRDD
+            :param sample: Fraction to sample GenotypeRDD. Should be between 0 and 1
+        """
+
+        self.sample = sample
+        new_col1 = when((col("alleles")[0] != u'NO_CALL') ,1) \
+            .otherwise(when( (col("alleles")[0] == u'NO_CALL'),2))
+
+        callrateData = genotypeRDD.toDF().sample(False, self.sample) \
+               .withColumn("calledstatus", new_col1) \
+               .groupBy("sampleid","calledstatus").count().collect()
+
+        data_called = {}
+        data_missing = {}
+        for row in callrateData:
+            curr = row.asDict()
+            if(curr['calledstatus'] == 1):
+                data_called[curr['sampleid']] = curr['count']
+            if(curr['calledstatus'] == 2):
+                data_missing[curr['sampleid']] = curr['count']
+
+        self.callRates = []
+        for sampleid in data_called.keys():
+            count_called = float(data_called.get(sampleid, 0))
+            count_missing = float(data_missing.get(sampleid, 0))
+            if(count_called > 0):
+                callrate = count_called /  ( count_called + count_missing)
+                self.callRates.append(callrate)
+
+
+    def plot(self, testMode = False, **kwargs):
+        """
+        Plots final distribution values and returns the plotted distribution as a list
+
+        Args:
+         :param testMode: if true, does not generate plot. Used for testing.
+
+        Returns:
+           matplotlib axis to plot and computed data
+        """
+
+        if(not testMode):
+            figsize = kwargs.get('figsize',(10,5))
+            fig, ax = plt.subplots(figsize=figsize)
+            hist = ax.hist(self.callRates,bins=100)
+            return ax, self.callRates
+        else:
+            return None, self.callRates
