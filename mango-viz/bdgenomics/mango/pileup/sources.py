b265cfd92437da5028a1a6d32be65430558f1072
Abhishek Mahesh <abhim00@berkeley.edu>
2019-06-12 11:26:20 -0400

    initial bed to json feature

--- mango-viz/bdgenomics/mango/pileup/sources.py
+++ mango-viz/bdgenomics/mango/pileup/sources.py
@@ -38,6 +38,7 @@ Sources specify where the genomic data comes from. Sources can come from a url, 
     GA4GHVariantSource
     GA4GHFeatureSource
 """
+
 from bdgenomics.mango.io import *
 
 # generic data source for pileup.js
@@ -93,9 +94,8 @@ class DataFrameSource(Source):
         
         #: name that pileup.js uses to identify sources --> temporary
         self.name = dataframe._pileup_visualization
-    
-    
-       
+
+        
 
 # can be used for TwoBit, vcf, BigBedDataSource, or BamDataSource
 class FileSource(Source):  
