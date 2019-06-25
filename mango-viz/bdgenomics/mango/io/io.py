8be9930012f64c7220467d2562e3e9e9eb335327
Devin Petersohn <devin.petersohn@gmail.com>
2019-06-25 11:37:40 -0700

    Restructure PR

---
+++ mango-viz/bdgenomics/mango/io/io.py
@@ -0,0 +1,10 @@
+from .bedfile import BedFile
+
+
+def read_bed(filepath_or_buffer,
+    column_names=["chrom","chromStart", "chromEnd", "name", "score",
+    "strand", "thickStart", "thickEnd", "itemRGB", "blockCount",
+    "blockSizes", "blockStarts"],
+    skiprows=None
+ ):
+    return BedFile.read(filepath_or_buffer, column_names, skiprows)
