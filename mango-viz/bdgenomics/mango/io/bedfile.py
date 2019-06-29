5be4b10d9c37aede8be07d354f2de0cbc48fc852
Abhishek Mahesh <abhim00@berkeley.edu>
2019-06-29 17:16:24 -0400

    updated io module with bedfile implementations

--- mango-viz/bdgenomics/mango/io/bedfile.py
+++ mango-viz/bdgenomics/mango/io/bedfile.py
@@ -9,8 +9,42 @@ class BedFile(GenomicFile):
 
     @classmethod
     def _parse(cls, df):
-        raise NotImplementedError("Implement Me!")
+        #check whether correct column names are passed into dataframe
+        df_cols = list(df.columns)
+        valid_columns = True
+        for name in ("chrom", "chromStart", "chromEnd"):
+            if name not in df_cols:
+                valid_columns = False
+
+        if not valid_columns:
+            #assume no names passed in and take first 3 columns as chrom, chromStart, chromEnd
+            chrom, chrom_start, chom_end = df_cols[:3]
+
+            chrom_starts = [int(chrom_start)] + (list(df[chrom_start]))
+            chrom_ends = [int(chom_end)] + (list(df[chom_end]))
+            chromosomes = [chrom] + (list(df[chrom]))
+
+            return build_json_from_bed(chrom_starts, chrom_ends, chromosomes)
+        else:
+            chromosomes = list(df["chrom"])
+            chrom_starts = list(df["chromStart"])
+            chrom_ends = list(df["chromEnd"])
+        #raise NotImplementedError("Implement Me!")
+        return (chrom_starts, chrom_ends, chromosomes)
 
     @classmethod
     def _to_json(cls, df):
-        raise NotImplementedError("Implement Me!")
+        chrom_starts, chrom_ends, chromosomes = cls._parse(df)
+        json_ga4gh = "{\"features\":["
+        for i in range(len(chromosomes)+1):
+            if i < len(chromosomes):
+                bed_content = "\"referenceName\":{}, \"start\":{}, \"end\":{}".format("\""+chromosomes[i]+"\"", "\""+str(chrom_starts[i])+"\"", "\""+str(chrom_ends[i])+"\"")
+                json_ga4gh = json_ga4gh + "{" + bed_content + "},"
+            else:
+                json_ga4gh = json_ga4gh[:len(json_ga4gh)-1]
+
+
+        #ending json
+        json_ga4gh = json_ga4gh + "]}"
+        return json_ga4gh
+        #raise NotImplementedError("Implement Me!")
