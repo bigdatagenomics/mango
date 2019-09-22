947cb807ce6137fc15ccac812d8a0b600836cc13
akmorrow13 <akmorrow@berkeley.edu>
2019-09-22 15:53:35 -0700

    update reference endpoint for widgets (#532)

--- mango-viz/bdgenomics/mango/js/src/utils.js
+++ mango-viz/bdgenomics/mango/js/src/utils.js
@@ -1,6 +1,8 @@
 /* @flow */
 
-var genomeBuilds = {'hg18': 'http://www.biodalliance.org/datasets/hg18.2bit', 'hg19': 'http://www.biodalliance.org/datasets/hg19.2bit', 'hg38': 'http://www.biodalliance.org/datasets/hg38.2bit'};
+var genomeBuilds = {'hg18': 'https://hgdownload.cse.ucsc.edu/goldenPath/hg18/bigZips/hg18.2bit',
+'hg19': 'https://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.2bit',
+'hg38': 'https://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.2bit'};
 
 module.exports = {
   genomeBuilds
